bd9e52407a1c771333de163bf59b30c24e80a705
Bree Xu <bree@Mattis-iMac.home>
2021-10-17 22:19:57 +0300

    Improved geometry

--- application.py
+++ application.py
@@ -34,37 +34,40 @@ def myClick():
     for item in result:
         T.insert(tk.INSERT, "{repo} : {number}\n".format(repo=item[0],number=item[1]))
     T.insert(tk.END, "That is all!")
-    
+
+#Create GUI with tkinter
+
+#Create main window and the title
 window = tk.Tk()
 window.title("Welcome to GitHub Repository Analysis app!")
 
 #Create label and entry widget for owner input
 lbl1 =tk.Label(window, text="Enter the owner name that you are interested (e.g. PowerShell):")
-lbl1.pack()
+lbl1.grid(row=0, column=0)
 
-entry_owner = tk.Entry(window, fg="black", bg="white", width=30)
-entry_owner.pack()
+entry_owner = tk.Entry(window, fg="black", bg="white", width=80)
+entry_owner.grid(row=0, column=1)
 
 #Create label and entry widget for authentication token
-lbl2 =tk.Label(window, text="Enter the username of your own GitHub username:")
-lbl2.pack()
+lbl2 =tk.Label(window, text="Enter your own GitHub username for authentication:")
+lbl2.grid(row=1, column=0)
 
-entry_user = tk.Entry(window, fg="black", bg="white", width=30)
-entry_user.pack()
+entry_user = tk.Entry(window, fg="black", bg="white", width=80)
+entry_user.grid(row=1,column=1)
 
 #Create label and entry widget for username input for authentication use
-lbl3 =tk.Label(window, text="Enter your own token key:")
-lbl3.pack() 
+lbl3 =tk.Label(window, text="Enter your own git token key:")
+lbl3.grid(row=2, column=0) 
 
-entry_token = tk.Entry(window, fg="black", bg="white", width=100)
-entry_token.pack()
+entry_token = tk.Entry(window, fg="black", bg="white", width=80)
+entry_token.grid(row=2, column=1)
 
 #Create button widget and define size and action
 btn = tk.Button(window, text="Search", padx=10, pady=5, fg="white", command=myClick)
-btn.pack()
+btn.grid(row=3, columnspan=2)
 
 #Create text widget and specify size
 T = tk.Text(window, height=50, width=50,bg="#263D42")
-T.pack()
+T.grid(row=4, columnspan=2)
 
 window.mainloop()
